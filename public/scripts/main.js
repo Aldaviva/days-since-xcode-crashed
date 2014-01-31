32660158de7aef339e06b7a8c890d5579c4f28b5
Ben Hutchison <ben@bluejeans.com>
2014-01-31 14:10:45 -0800

    Extracted achievement list to separate file because it changes often.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,33 +1,6 @@
 var API_ROOT = "/cgi-bin/";
 var CRASH_NAME = "xcode";
 
-var ACHIEVEMENTS = [
-	 { name: "contra speedrun",            hours:    0.168 },
-	 { name: "transcontinental blackbird", hours:    1.132 },
-	 { name: "energetic",                  hours:    5.000 },
-	 { name: "business day",               hours:    8.000 },
-	 { name: "NSRangeException",           hours:   10.000 },
-	 { name: "daily restart",              hours:   24.000 },
-	 { name: "cannonball run",             hours:   28.842 },
-	 { name: "a full deck",                hours:   52.000 },
-	 { name: "around the world",           hours:   67.017 }, //http://en.wikipedia.org/wiki/Virgin_Atlantic_GlobalFlyer
-	 { name: "long weekend",               hours:   72.000 },
-	 { name: "Oh the humanity!",           hours:   77.133 },
-	 { name: "titanic voyage",             hours:   87.800 },
-	 { name: "to the moon",                hours:  102.750 },
-	 { name: "eww",                        hours:  144.000 },
-	 { name: "24/7",                       hours:  168.000 },
-	 { name: "headcount",                  hours:  195.000 },
-	 { name: "megasecond",                 hours:  277.778 },
-	 { name: "fortnight",                  hours:  336.000 },
-	 { name: "Xcode not found",            hours:  404.000 },
-	 { name: "lunar cycle",                hours:  708.734 },
-	 { name: "odometer rollover",          hours: 1000.000 },
-	 { name: "updates available",          hours: 2064.000 },
-	 { name: "revolution of mercury",      hours: 2111.111 },
-	 { name: "Napoleon's Hundred Days",    hours: 2280.000 }
-];
-
 var mostRecentCrashDate;
 
 main();
